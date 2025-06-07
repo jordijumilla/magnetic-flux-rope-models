9c6b7dcab2e4accf74fdb4cb90c0081e9999cfaa
Jordi Jumilla Lorenz <jordijuud@gmail.com>
2025-06-07 20:08:39 +0100

    Updated elliptical unit tests

--- test/test_elliptical_coordinates.py
+++ test/test_elliptical_coordinates.py
@@ -17,16 +17,35 @@ all_param_combinations: list = list(itertools.product(R_range, delta_range, psi_
 test_tolerance: float = 1e-8
 
 @pytest.mark.parametrize("R, delta, psi", all_param_combinations)
-def test_identity(R: float, delta: float, psi: float) -> None:
+def test_identity_change_of_coordinates_cartesian_to_elliptical(R: float, delta: float, psi: float) -> None:
+    # Create an instance of the model with the given R, delta and psi.
     model = EllipticalCylindricalModel(R=R, delta=delta, psi=psi)
+    
+    # Convert the cartesian coordinates (0, R, 0) to elliptical coordinates.
     x = 0.0
     y = model.R
     z = 0.0
-
     elliptical_coordinates = model.convert_cartesian_to_elliptical_coordinates(x, y, z)
+
+    # Convert the elliptical coordinates back to Cartesian coordinates.
     cartesian_coordinates = model.convert_elliptical_to_cartesian_coordinates(elliptical_coordinates[0], elliptical_coordinates[1], elliptical_coordinates[2])
 
+    # Check that we recover the original Cartesian coordinates.
     assert np.allclose([x, y, z], cartesian_coordinates, atol=test_tolerance)
+
+@pytest.mark.parametrize("R, delta, psi", all_param_combinations)
+def test_identity_change_of_coordinates_elliptical_to_cartesian(R: float, delta: float, psi: float) -> None:
+    # Create an instance of the model with the given R, delta and psi.
+    model = EllipticalCylindricalModel(R=R, delta=delta, psi=psi)
+
+    # Convert the elliptical coordinates (R, 0, 0) to cartesian coordinates.
+    cartesian_coordinates = model.convert_elliptical_to_cartesian_coordinates(r=model.R, phi=0.0, z=0.0)
+
+    # Convert the Cartesian coordinates back to elliptical coordinates.
+    elliptical_coordinates = model.convert_cartesian_to_elliptical_coordinates(cartesian_coordinates[0], cartesian_coordinates[1], cartesian_coordinates[2])
+
+    # Check that we recover the original elliptical coordinates.
+    assert np.allclose([model.R, 0, 0], elliptical_coordinates, atol=test_tolerance)
 
 def get_quadrant_point(R: float, delta: float, psi: float, quadrant: int) -> tuple[float, float]:
     """Get a point in the specified quadrant of the elliptical elliptical-cylindrical coordinates."""
@@ -53,10 +72,10 @@ def test_quadrants(R: float, delta: float, psi: float) -> None:
     model = EllipticalCylindricalModel(delta=delta, psi=psi, R=R)
 
     # Test each quadrant.
-    for quadrant, expected_phi in zip([1, 2, 3, 4], [0, math.pi/2, math.pi, -math.pi/2]):
+    for quadrant, expected_phi in zip([1, 2, 3, 4], [0, math.pi/2, math.pi, 3*math.pi/2]):
         x, y = get_quadrant_point(R=R, delta=delta, psi=psi, quadrant=quadrant)
 
-        ellip = model.convert_cartesian_to_elliptical_coordinates(x, y, 0.0)
+        ellip = model.convert_cartesian_to_elliptical_coordinates(x=x, y=y, z=0.0)
         r = ellip[0]
         phi = ellip[1]
         z_ellip = ellip[2]
@@ -91,6 +110,15 @@ def test_elliptical_to_cartesian(R: float, delta: float, psi: float) -> None:
             assert np.isclose(p_cartesian[1], expected_coordinates[1], atol=test_tolerance)
             assert np.isclose(p_cartesian[2], z, atol=test_tolerance)
 
+@pytest.mark.parametrize("R, delta, psi", all_param_combinations)
+def test_elliptical_unit_basis_scalar(R: float, delta: float, psi: float) -> None:
+    # Create an instance of the EllipticalCylindricalModel with specific parameters.
+    ec_model = EllipticalCylindricalModel(R=R, delta=delta, psi=psi)
 
-def test_elliptical_to_cartesian_vector() -> None:
-    pass
+    # Get the elliptical basis vectors at r=R and phi=0.
+    unit_basis = ec_model.get_elliptical_basis(r=ec_model.R, phi=0.0, unit_basis=True)
+
+    # Check that the basis vectors are the expected theoretical values.
+    assert np.allclose(unit_basis[:, 0], np.array([np.cos(ec_model.psi), np.sin(ec_model.psi), 0]), atol=1e-8)
+    assert np.allclose(unit_basis[:, 1], np.array([-np.sin(ec_model.psi), np.cos(ec_model.psi), 0]), atol=1e-8)
+    assert np.allclose(unit_basis[:, 2], np.array([0, 0, 1]), atol=1e-8)
